510e43b380dfbbe3b01faacf3abb7c9edf7d7b40
silask <silas.kieser@gmail.com>
2021-05-12 16:36:54 +0200

    build

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,24 @@
 
 set -e
 
+commit_msg=$(git log -1 --pretty=%B)
+echo "last message was "$commit_msg
+
 echo Build
 
 hugo
 
 echo add Build to git submodule
 
+
 cd public
 git add .
-git commit -m "Build website"
+
+
+
+
+
+git commit -m"$commit_msg"
 
 echo "Deploy"
 
