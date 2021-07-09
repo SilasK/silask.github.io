83dcb871efff35d283047d2cacd93cb974669ecf
silask <silas.kieser@gmail.com>
2021-07-09 11:48:03 +0200

    add new publications

--- view.sh
+++ view.sh
@@ -4,14 +4,14 @@ process=$(lsof -i :1313 -Fp |head -n1)
 
 #if ![ -z ${var+x} ];
 process="${process:1}"
-kill -STOP $process
+kill -9 $process
 
 
 
 
 
 open -a "Google Chrome.app" "http://localhost:1313"
-hugo --i18n-warnings server 
+hugo --i18n-warnings server
 #
 # process=$(lsof -i :1313 -Fp |head -n1)
 #
