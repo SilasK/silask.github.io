07c3c815ce30e08c04b61e0fd84bd9ec4113a122
silask <silas.kieser@gmail.com>
2021-07-09 11:37:56 +0200

    new view

--- view.sh
+++ view.sh
@@ -4,11 +4,18 @@ process=$(lsof -i :1313 -Fp |head -n1)
 
 #if ![ -z ${var+x} ];
 process="${process:1}"
-kill -9 $process
+kill -STOP $process
 
 
-hugo --i18n-warnings server &
+
+
 
 open -a "Google Chrome.app" "http://localhost:1313"
-
-fg
+hugo --i18n-warnings server 
+#
+# process=$(lsof -i :1313 -Fp |head -n1)
+#
+# #if ![ -z ${var+x} ];
+# process="${process:1}"
+#
+# fg $process
